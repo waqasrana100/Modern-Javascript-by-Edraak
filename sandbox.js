cff623e593f5174338d9b591713d2f40ac27c076
unknown <iamranawaqas100@gmail.com>
2022-05-03 02:24:52 +0500

    variable and constants done

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,13 @@
-console.log(1);
-console.log(2);
+//this is age
+let age = 25;
+//this is year
+let year = 2022
+const points = 20;
+console.log(age, year, points);
+
+// age =30
+console.log(age);
+
+var score = 40;
+
+console.log(score)
